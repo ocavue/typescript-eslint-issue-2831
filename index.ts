3d78c67312204ce8ff1811c05777e1702a592541
ocavue <ocavue@gmail.com>
2020-12-01 14:29:12 +0800

    init

---
+++ index.ts
@@ -0,0 +1,17 @@
+const classes = [
+    class C1 {
+
+    },
+    class C2 {
+
+    },
+    class C3 {
+
+    },
+]
+
+export function printClasses() {
+    for (let cls of classes) {
+        console.log(cls)
+    }
+}
