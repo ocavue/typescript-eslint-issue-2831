86e8b6f6fa5dc1a0c288234d39c1d88ab38eddbe
ocavue <ocavue@gmail.com>
2020-12-03 19:57:13 +0800

    update

--- index.ts
+++ index.ts
@@ -1,23 +1,9 @@
-export const classes = [
-    class C1 {
-    },
-    class C2 {
-    },
-    class C3 {
-    },
-]
+export const arrayWithClasses = [class C1 { }]
 
 export const objectWithClasses = {
     controller: class MyClass { }
 }
 
-export const a = (): unknown => {
+export const functionReturnsClass = (): unknown => {
     return class A {}
-}
-
-export function printClasses(): void {
-    for (const cls of classes) {
-        const obj = new cls()
-        console.log(obj)
-    }
 }
